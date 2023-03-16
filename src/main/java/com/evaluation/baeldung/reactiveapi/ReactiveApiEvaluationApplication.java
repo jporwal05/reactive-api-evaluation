7de4e13190190068dd042dd53564e725c3249619
jporwal05 <jayantporwal05@gmail.com>
2023-03-17 01:09:29 +0530

    add: starter project for webflux

---
+++ src/main/java/com/evaluation/baeldung/reactiveapi/ReactiveApiEvaluationApplication.java
@@ -0,0 +1,13 @@
+package com.evaluation.baeldung.reactiveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApiEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveApiEvaluationApplication.class, args);
+	}
+
+}
